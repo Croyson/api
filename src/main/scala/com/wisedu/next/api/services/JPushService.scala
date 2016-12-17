894046654acf460fddc60082e6bbf59bd5c70444
Croyson <317335593@qq.com>
2016-12-17 14:32:26 +0800

    增加消息提示功能

--- src/main/scala/com/wisedu/next/api/services/JPushService.scala
+++ src/main/scala/com/wisedu/next/api/services/JPushService.scala
@@ -1,4 +1,4 @@
-package com.wisedu.next.admin.services
+package com.wisedu.next.api.services
 
 import javax.inject.{Inject, Singleton}
 
@@ -7,21 +7,23 @@ import com.twitter.finatra.json.FinatraObjectMapper
 import com.wisedu.next.annotations.JPushHttpClient
 
 import scala.collection.mutable.{Map => MutMap}
-case class Extras(newFeed:String)
+
+case class Extras(newFeed: String)
 
 case class AndroidNotification(alert: String, title: String, extras: Extras)
 
-case class IosNotification(alert: String, extras: Extras,sound:String,badge:Int)
+case class IosNotification(alert: String, extras: Extras, sound: String, badge: Int)
 
 case class PushOptions(time_to_live: Int, apns_production: Boolean)
 
 case class Notification(android: Option[AndroidNotification], ios: Option[IosNotification])
 
-case class Message(msg_content:String,title:String,content_type:String,extras:Extras)
+case class Message(msg_content: String, title: String, content_type: String, extras: Extras)
 
 case class Audience(tag: Option[Seq[String]], tag_and: Option[Seq[String]], alias: Option[Seq[String]])
 
 case class PushResponse(sendno: String, msg_id: String)
+
 @Singleton
 class JPushService {
   @Inject
@@ -33,14 +35,14 @@ class JPushService {
     val extras = Extras("1")
     //val iosNotification = IosNotification("", extras,"sound.caf",1)
     //val androidNotification = AndroidNotification("","", extras)
-   // val notification = Notification(Some(androidNotification), Some(iosNotification))
-    val message = Message("有新的新鲜事","msg","text",extras)
+    // val notification = Notification(Some(androidNotification), Some(iosNotification))
+    val message = Message("有新的新鲜事", "msg", "text", extras)
     val platform = "all"
-    val options = PushOptions(86400,true)
-    val msg = MutMap("platform" -> platform, "message" -> message, "options" -> options,"audience" -> Audience(Some(tags), None, None))
+    val options = PushOptions(86400, true)
+    val msg = MutMap("platform" -> platform, "message" -> message, "options" -> options, "audience" -> Audience(Some(tags), None, None))
     val body = objectMapper.writePrettyString(msg)
     jPushHttpClient.execute(RequestBuilder.post("/v3/push").headers(headers).body(body)).map {
-      ret =>PushResponse(ret.getContentString(),tags.mkString(","))
+      ret => PushResponse(ret.getContentString(), tags.mkString(","))
     }
 
   }
